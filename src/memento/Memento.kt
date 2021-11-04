7f36dc6d843406d3b4f6380a07064ed390fb98d2
mohamedtamer0 <mt4452242@gmail.com>
2021-11-05 00:32:16 +0200

    update

---
+++ src/memento/Memento.kt
@@ -0,0 +1,4 @@
+package memento
+
+
+data class Memento(val state: String)
---
+++ src/memento/Originator.kt
@@ -0,0 +1,16 @@
+package memento
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+class Originator(var state: String) {
+
+    fun createMemento(): Memento {
+        return Memento(state)
+    }
+
+    fun restore(memento: Memento) {
+        state = memento.state
+    }
+}
